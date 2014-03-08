1a159a8ba6a94d08d3e4399a25ce698cdd0fdd42
Ryohei Ikegami <iofg2100@gmail.com>
2014-03-08 15:28:54 +0900

    Add destroy: option to QtObjectBase.new

--- lib/qml/qt_object_base.rb
+++ lib/qml/qt_object_base.rb
@@ -2,21 +2,16 @@ require 'qml/c_lib'
 module QML
   class QtObjectBase
 
-    def initialize(ptr)
-      self.pointer = ptr
-      @_gc_protected = []
-    end
+    attr_reader :pointer
 
-    def pointer
-      @_pointer
-    end
-
-    def pointer=(ptr)
-      @_pointer = FFI::AutoPointer.new(ptr, CLib.method(:qobject_destroy))
+    def initialize(ptr, destroy: true)
+      fail TypeError, 'ptr must be a FFI::Pointer' unless ptr.is_a? FFI::Pointer
+      @pointer = destroy ? FFI::AutoPointer.new(ptr, CLib.method(:qobject_destroy)) : ptr
+      @gc_protected = []
     end
 
     def gc_protect(obj)
-      @_gc_protected << obj
+      @gc_protected << obj
     end
 
   end

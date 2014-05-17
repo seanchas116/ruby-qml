13163b7bcb159a028ffd23e641d705cd786ae60c
Ryohei Ikegami <iofg2100@gmail.com>
2014-05-17 13:35:39 +0900

    Use let in RSpec

--- spec/plugin_loader_spec.rb
+++ spec/plugin_loader_spec.rb
@@ -2,22 +2,25 @@ require 'spec_helper'
 
 describe QML::PluginLoader do
 
+  let(:loader) { QML::PluginLoader.new path, 'rubyqml-testobject' }
+
   describe 'load' do
+
     context 'with correct file path' do
+
+      let(:path) { QML::ROOT_PATH + 'ext/qml/plugins/testobject' }
       it 'creates an object instance' do
-        path = QML::ROOT_PATH + 'ext/qml/plugins/testobject'
-        loader = QML::PluginLoader.new path, 'rubyqml-testobject'
         loader.load
         expect(loader.instance).to be_a QML::ObjectBase
       end
     end
+
     context 'with wrong file path' do
+
+      let(:path) { QML::ROOT_PATH + 'ext/qml/plugins/wrong' }
       it 'fails with QML::PluginError' do
-        path = QML::ROOT_PATH + 'ext/qml/plugins/wrong'
-        loader = QML::PluginLoader.new path, 'rubyqml-testobject'
         expect { loader.load }.to raise_error(QML::PluginError)
       end
     end
   end
-
 end
--- spec/test_object_spec.rb
+++ spec/test_object_spec.rb
@@ -2,27 +2,24 @@ require 'spec_helper'
 
 describe "test object" do
 
-  before do
-    loader = QML::PluginLoader.new QML::ROOT_PATH + 'ext/qml/plugins/testobject', 'rubyqml-testobject'
-    @obj = loader.instance.createTestObject
-    @subobj = loader.instance.createTestObjectSubclass
-  end
+  let(:loader) { QML::PluginLoader.new QML::ROOT_PATH + 'ext/qml/plugins/testobject', 'rubyqml-testobject' }
+  let(:obj) { loader.instance.createTestObject }
 
   describe '#normalMethod' do
 
     it 'should call the method' do
-      expect(@obj.normalMethod(1, "23")).to eq(24)
+      expect(obj.normalMethod(1, "23")).to eq(24)
     end
 
     context 'if the arguments are not convertible' do
       it 'should raise error' do
-        expect { @obj.normalMethod([1, 2], 'bar') }.to raise_error QML::MethodError
+        expect { obj.normalMethod([1, 2], 'bar') }.to raise_error QML::MethodError
       end
     end
 
     context 'if the number of arguments is wrong' do
       it 'should raise error' do
-        expect { @obj.normalMethod(1) }.to raise_error QML::MethodError
+        expect { obj.normalMethod(1) }.to raise_error QML::MethodError
       end
     end
   end
@@ -30,20 +27,20 @@ describe "test object" do
   describe '#variantMethod' do
 
     it 'should call the method with QVariant arguments' do
-      expect(@obj.variantMethod(20, [1, 3, 4, 9])).to eq(37)
+      expect(obj.variantMethod(20, [1, 3, 4, 9])).to eq(37)
     end
   end
 
   describe '#overloadedMethod' do
 
     it 'should call the method with the same arity' do
-      expect(@obj.overloadedMethod('foo', 1)).to eq('2 params')
+      expect(obj.overloadedMethod('foo', 1)).to eq('2 params')
     end
 
     context 'when multiple methods with the same arity are provided' do
 
       it 'should call the method defined last' do
-        expect(@obj.overloadedMethod('hoge')).to eq('last')
+        expect(obj.overloadedMethod('hoge')).to eq('last')
       end
     end
   end
@@ -52,10 +49,10 @@ describe "test object" do
 
     it 'should emit the signal' do
       result = nil
-      @obj.someSignal.connect do |str|
+      obj.someSignal.connect do |str|
         result = str
       end
-      @obj.emitSomeSignal('poyopoyo')
+      obj.emitSomeSignal('poyopoyo')
       expect(result).to eq 'poyopoyo'
       # spy = Ropework::SignalSpy.new(@obj.someSignal)
       # @obj.emitSomeSignal('poyopoyo')
@@ -65,39 +62,39 @@ describe "test object" do
 
   describe '#throwingMethod' do
     it 'raises a QML::CxxError' do
-      expect { @obj.throwingMethod }.to raise_error(QML::CxxError, "<std::runtime_error> test error")
+      expect { obj.throwingMethod }.to raise_error(QML::CxxError, "<std::runtime_error> test error")
     end
   end
 
   describe '#metaObjectMethod' do
     it 'returns the QML::MetaObject of the test object' do
-      expect(@obj.metaObjectMethod).to eq @obj.class.meta_object
+      expect(obj.metaObjectMethod).to eq obj.class.meta_object
     end
   end
 
   describe '#selfReturningMethod' do
     it 'returns the same Ruby object' do
-      expect(@obj.selfReturningMethod).to be @obj
+      expect(obj.selfReturningMethod).to be obj
     end
   end
 
   describe 'name property' do
 
     it 'should be set' do
-      @obj.name = 'abcd'
-      expect(@obj.getName).to eq('abcd')
+      obj.name = 'abcd'
+      expect(obj.getName).to eq('abcd')
     end
 
     it 'should notify when changed' do
-      spy = Ropework::SignalSpy.new(@obj.name_changed)
-      @obj.name = 'hogepoyo'
+      spy = Ropework::SignalSpy.new(obj.name_changed)
+      obj.name = 'hogepoyo'
       expect(spy.args.first).to eq(['hogepoyo'])
     end
 
     context 'with wrong type' do
 
       it 'raise error' do
-        expect { @obj.name = 123 }.to raise_error QML::PropertyError
+        expect { obj.name = 123 }.to raise_error QML::PropertyError
       end
     end
   end
@@ -105,7 +102,7 @@ describe "test object" do
   describe 'enums' do
     
     it 'should be set' do
-      k = @obj.class
+      k = obj.class
       expect(k::Apple).to eq(0)
       expect(k::Banana).to eq(1)
       expect(k::Orange).to eq(2)
@@ -113,8 +110,9 @@ describe "test object" do
   end
 
   context 'in subclass' do
+    let(:subobj) { loader.instance.createTestObject }
     it "can call TestObject's method" do
-      expect(@subobj.normalMethod(1, "23")).to eq(24)
+      expect(subobj.normalMethod(1, "23")).to eq(24)
     end
   end
 end

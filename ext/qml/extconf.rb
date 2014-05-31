4fed357f26c866cf7527b9e9028150859ee46698
Ryohei Ikegami <iofg2100@gmail.com>
2014-05-31 22:01:33 +0900

    Improve extconf.rb

--- ext/qml/extconf.rb
+++ ext/qml/extconf.rb
@@ -1,15 +1,19 @@
 require 'mkmf'
 require 'pathname'
+$LOAD_PATH.unshift Pathname(__FILE__) + '../../../lib'
+require 'qml/platform'
 
 qt_include, qt_lib = dir_config('qt')
-qmake = qt_include ? Pathname(qt_include).realpath.parent.+('bin/qmake') : 'qmake'
+qt_path =
+  if qt_include
+    Pathname(qt_include).realpath.parent
+  else
+    qmake_path = `which qmake`.strip
+    fail "qmake not found" if qmake_path == ''
+    Pathname(qmake_path).parent.parent
+  end
 
-RbConfig::CONFIG['CPP'].gsub!(RbConfig::CONFIG['CC'], RbConfig::CONFIG['CXX'])
-
-headers = ['QtQuick/QQuickView']
-headers.each do |header|
-  fail "header not found: #{header}" unless have_header(header)
-end
+qmake = qt_path + 'bin/qmake'
 
 # build plugins
 
@@ -22,9 +26,41 @@ end
 
 # build extension itself
 
-$CPPFLAGS += ' -std=c++11 -Wall -Wextra -g'
+RbConfig::CONFIG['CPP'].gsub!(RbConfig::CONFIG['CC'], RbConfig::CONFIG['CXX'])
 
-# TODO: add support for other platforms than Mac
-$LDFLAGS += " -F#{qt_lib} -framework QtCore -framework QtTest"
+case
+when QML::Platform.mac?
+  framework_flag = " -F'#{qt_path + 'lib'}'"
+  $CPPFLAGS += framework_flag
+  $LDFLAGS += framework_flag
+else
+  $LDFLAGS += " -L'#{qt_path + 'lib'}'"
+end
+
+modules = %w{QtCore QtGui QtQml QtQuick}
+modules.each do |mod|
+  case
+  when QML::Platform.mac?
+    $CPPFLAGS += " -I'#{qt_path + "lib/#{mod}.framework/Headers"}'"
+    $LDFLAGS += " -framework #{mod}"
+  else
+    $CPPFLAGS += " -I'#{include_path + mod}'"
+    $LDFLAGS += " -l#{mod.downcase}"
+  end
+end
+
+headers = %w{
+  QtCore/QObject
+  QtCore/QVariant
+  QtGui/QGuiApplication
+  QtQml/QQmlEngine
+  QtQml/QQmlComponent
+  QtQml/QQmlContext
+}
+headers.each do |h|
+  fail "header not found: #{h}" unless have_header(h)
+end
+
+$CPPFLAGS += ' -std=c++11 -Wall -Wextra -g'
 
 create_makefile "qml/qml"

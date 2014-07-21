63b419e5a07b956255c18b83e9c2eccf05ae0c14
Ryohei Ikegami <iofg2100@gmail.com>
2014-07-21 19:40:45 +0900

    Revert "Fix redefinition warnings in ClassBuilder"
    This reverts commit 8dcce4f04d82ce757de1ef925b03776c156dde5b.

--- lib/qml/class_builder.rb
+++ lib/qml/class_builder.rb
@@ -56,24 +56,18 @@ module QML
 
     def build
       create unless @klass
-      return self if @klass.meta_object == @metaobj
+      return if @klass.meta_object == @metaobj
 
-      properties = @metaobj.property_names
-      notifiers = properties.map { |name| @metaobj.notify_signal(name) }
-      signals = @metaobj.method_names.select { |name| @metaobj.signal?(name) } - notifiers
-      methods = @metaobj.method_names.reject { |name| @metaobj.signal?(name) }
-      enums = @metaobj.enumerators
-
-      properties.each do |name|
-        define_property(name)
-      end
-      signals.each do |name|
-        define_signal(name)
-      end
-      methods.each do |name|
+      @metaobj.method_names.reject { |name| @metaobj.signal?(name) }.each do |name|
         define_method(name)
       end
-      enums.each do |k, v|
+      @metaobj.method_names.select { |name| @metaobj.signal?(name) }.each do |name|
+        define_signal(name)
+      end
+      @metaobj.property_names.each do |name|
+        define_property(name)
+      end
+      @metaobj.enumerators.each do |k, v|
         define_enum(k, v)
       end
       @klass.__send__ :meta_object=, @metaobj
@@ -91,7 +85,6 @@ module QML
     def define_method(name)
       metaobj = @metaobj
       return if metaobj.private?(name)
-      return if @klass.instance_methods.include?(name)
       @klass.__send__ :define_method, name do |*args|
         metaobj.invoke_method(@pointer, name, args)
       end
@@ -101,7 +94,6 @@ module QML
     end
 
     def define_signal(name)
-      return if @klass.instance_signals.include?(name)
       @klass.__send__ :variadic_signal, name, factory: proc { |obj|
         QtSignal.new(@metaobj, obj.pointer, name)
       }
@@ -110,7 +102,6 @@ module QML
     end
 
     def define_property(name)
-      return if @klass.instance_properties.include?(name)
       metaobj = @metaobj
       @klass.__send__ :property, name, factory: proc { |obj|
         QtProperty.new(@metaobj, obj.pointer, name)

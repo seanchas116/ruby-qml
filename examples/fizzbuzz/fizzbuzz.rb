ffbcc1a79342357795ae4efb26e62f8d9f5dd4f1
riocampos <riocampos22@gmail.com>
2014-08-01 12:16:26 +0900

    0 is not Fizz, Buzz, and FizzBuzz

--- examples/fizzbuzz/fizzbuzz.rb
+++ examples/fizzbuzz/fizzbuzz.rb
@@ -1,10 +1,9 @@
-$LOAD_PATH.unshift File.expand_path('../../../lib', __FILE__)
 require 'qml'
 require 'pathname'
 
 module Examples
   module FizzBuzz
-    VERSION = '0.1'
+    VERSION = '0.1.1'
 
     class FizzBuzz
       include QML::Access
@@ -16,12 +15,12 @@ module Examples
       on_changed :input do
         i = input.to_i
         self.result = case
-        when i % 15 == 0
+        when i % 15 == 0 && i.nonzero?
           inputWasFizzBuzz.emit
           "FizzBuzz"
-        when i % 3 == 0
+        when i % 3 == 0 && i.nonzero?
           "Fizz"
-        when i % 5 == 0
+        when i % 5 == 0 && i.nonzero?
           "Buzz"
         else
           i.to_s

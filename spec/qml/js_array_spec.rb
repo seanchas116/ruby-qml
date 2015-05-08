16628dad484fdfe657a05ca86e7f895e4701bb0c
Ryohei Ikegami <iofg2100@gmail.com>
2015-05-08 18:44:34 +0900

    Add JSArray spec

---
+++ spec/qml/js_array_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe QML::JSArray do
+
+  let(:engine) { QML::Engine.new }
+  let(:array_script) do
+    <<-JS
+      [1, 2, 3]
+    JS
+  end
+  let(:array) { engine.evaluate(array_script) }
+
+  describe '#each' do
+    it 'enumerates each values' do
+      expect(array.each.to_a).to eq [1,2,3]
+    end
+  end
+
+  describe '#length' do
+    it 'returns length' do
+      expect(array.length).to eq 3
+    end
+  end
+end

9fa8ae7f915eff2199f0fcdc6994928eecf5122f
Ryohei Ikegami <iofg2100@gmail.com>
2015-05-15 23:37:21 +0900

    Implement Application

--- lib/qml/application.rb
+++ lib/qml/application.rb
@@ -4,5 +4,67 @@ module QML
   #
   # @see http://qt-project.org/doc/qt-5/qapplication.html QApplication (C++)
   class Application
+
+    # @return [Engine] The engine of the application.
+    def engine
+      QML.engine
+    end
+
+    # @return [Component] The root component of the application that represents the loaded QML file.
+    # @see #load
+    # @see #load_data
+    # @see #load_path
+    def root_component
+      @root_component or fail ApplicationError, "QML data or file has not been loaded"
+    end
+
+    # Loads a QML file. The loaded component can be accessed by {#root_component}
+    # @param [Hash] opts
+    # @param [String] data
+    # @param [String] path
+    # @see Component
+    def load(data: nil, path: nil)
+      @root_component = Component.new(data: data, path: path)
+      @root = @root_component.create
+    end
+
+    # Loads a QML file from string data.
+    # @see #load
+    def load_data(data)
+      load(data: data)
+    end
+
+    # Loads a QML file from a file path.
+    # @see #load
+    def load_path(path)
+      load(path: path)
+    end
+
+    # @return The root object created by the root component.
+    def root
+      @root or fail "QML data or file has not been loaded"
+    end
+
+    # Runs garbage collection of Ruby and QML and deletes unused objects.
+    def collect_garbage
+      ::GC.start
+      engine.collect_garbage
+      process_events
+    end
   end
+
+  # Creates an {Application}, yields it and then call {Application#exec}.
+  # @return [Application]
+  # @example
+  #   QML.run do |app|
+  #     app.load_path Pathname(__FILE__) + '../main.qml'
+  #   end
+  def run
+    Application.new.tap do |app|
+      yield app
+      app.exec
+    end
+  end
+
+  module_function :run
 end
---
+++ spec/qml/application_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe QML::Application do
+  let(:application) { QML.application }
+
+  describe '#engine' do
+    it 'returns the default engine of the application' do
+      expect(application.engine).to be_a(QML::Engine)
+    end
+  end
+  describe '#load_data' do
+    let(:data) do
+      <<-EOS
+        import QtQuick 2.0
+        QtObject {
+          property string name: 'foo'
+        }
+      EOS
+    end
+    it 'loads root object 1from data' do
+      application.load_data data
+      expect(application.root_component.data).to eq data
+      expect(application.root.name).to eq 'foo'
+    end
+  end
+  describe '#load_path' do
+    let(:path) { QML::ROOT_PATH + 'spec/assets/testobj.qml' }
+    it 'loads root object from path' do
+      application.load_path path
+      expect(application.root_component.path).to eq path
+      expect(application.root.name).to eq 'foo'
+    end
+  end
+end
+
+describe QML do
+  describe '.application' do
+    it 'returns the QML::Application instance' do
+      expect(QML.application).to be_a(QML::Application)
+    end
+  end
+end

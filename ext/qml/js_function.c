89c222f91245fe473ae4fe71cf78dd710a63098f
Ryohei Ikegami <iofg2100@gmail.com>
2015-05-16 00:35:45 +0900

    Add JSFunction#new

--- ext/qml/js_function.c
+++ ext/qml/js_function.c
@@ -4,7 +4,11 @@
 
 VALUE rbqml_cJSFunction;
 
-static VALUE function_call(VALUE self, VALUE thisValue, VALUE args) {
+typedef enum {
+    CallFunction, CallMethod, CallConstructor
+} CallType;
+
+static VALUE function_call(VALUE self, VALUE thisValue, VALUE args, CallType callType) {
     qmlbind_value func = rbqml_js_object_get(self);
 
     int argc = RARRAY_LEN(args);
@@ -16,13 +20,23 @@ static VALUE function_call(VALUE self, VALUE thisValue, VALUE args) {
     }
 
     qmlbind_value result;
-    if (NIL_P(thisValue)) {
+
+    switch (callType) {
+    case CallFunction:
         result = qmlbind_value_call(func, argc, qmlArgs);
-    } else {
+        break;
+    case CallMethod: {
         qmlbind_value qmlThis = rbqml_js_object_get(thisValue);
         result = qmlbind_value_call_with_instance(func, qmlThis, argc, qmlArgs);
         qmlbind_value_release(qmlThis);
+        break;
     }
+    case CallConstructor: {
+        result = qmlbind_value_call_constructor(func, argc, qmlArgs);
+        break;
+    }
+    }
+
     bool is_error = qmlbind_value_is_error(result);
     VALUE resultValue = rbqml_to_ruby(result);
     qmlbind_value_release(result);
@@ -35,7 +49,11 @@ static VALUE function_call(VALUE self, VALUE thisValue, VALUE args) {
 }
 
 static VALUE js_function_call(VALUE self, VALUE args) {
-    return function_call(self, Qnil, args);
+    return function_call(self, Qnil, args, CallFunction);
+}
+
+static VALUE js_function_new(VALUE self, VALUE args) {
+    return function_call(self, Qnil, args, CallConstructor);
 }
 
 static VALUE js_function_call_with_instance(int argc, VALUE *argv, VALUE self) {
@@ -43,12 +61,13 @@ static VALUE js_function_call_with_instance(int argc, VALUE *argv, VALUE self) {
     VALUE args;
 
     rb_scan_args(argc, argv, "1*", &thisValue, &args);
-    return function_call(self, thisValue, args);
+    return function_call(self, thisValue, args, CallMethod);
 }
 
 void rbqml_init_js_function(void) {
     rbqml_cJSFunction = rb_define_class_under(rb_path2class("QML"), "JSFunction", rbqml_cJSObject);
 
     rb_define_method(rbqml_cJSFunction, "call", &js_function_call, -2);
+    rb_define_method(rbqml_cJSFunction, "new", &js_function_new, -2);
     rb_define_method(rbqml_cJSFunction, "call_with_instance", &js_function_call_with_instance, -1);
 }
--- spec/qml/js_function_spec.rb
+++ spec/qml/js_function_spec.rb
@@ -2,18 +2,31 @@ require 'spec_helper'
 
 describe QML::JSFunction do
 
-  let(:function_script) do
-    <<-JS
+  let(:function) do
+    QML.engine.evaluate <<-JS
       (function(a, b) {
         return a + b;
       });
     JS
   end
-  let(:function) { QML.engine.evaluate(function_script) }
+
+  let(:constructor) do
+    QML.engine.evaluate <<-JS
+      (function(a, b) {
+        this.value = a + b;
+      });
+    JS
+  end
 
   describe '#call' do
     it 'calls function' do
       expect(function.call(1,2)).to eq 3
+    end
+  end
+
+  describe '#new' do
+    it 'calls function as a constructor' do
+      expect(constructor.new(1, 2).value).to eq 3
     end
   end
 end

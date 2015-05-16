a472062fd4f3fdeaa661edc2b583c1228928beb4
Ryohei Ikegami <iofg2100@gmail.com>
2015-05-16 19:11:18 +0900

    Remove unused error classes

--- lib/qml/errors.rb
+++ lib/qml/errors.rb
@@ -1,16 +1,5 @@
 module QML
-
-  class MethodError < StandardError; end
-  class SignalError < StandardError; end
-  class PropertyError < StandardError; end
   class PluginError < StandardError; end
-  class QtObjectError < StandardError; end
-  class ConversionError < StandardError; end
   class QMLError < StandardError; end
   class AccessError < StandardError; end
-  class ApplicationError < StandardError; end
-  class EngineError < StandardError; end
-  class InvalidThreadError < StandardError; end
-  class UninitializedError < StandardError; end
-  class AlreadyInitializedError < StandardError; end
 end

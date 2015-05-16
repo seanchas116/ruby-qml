ce6d84586c29ddcd3c5d888a1c1083b34aadc603
Ryohei Ikegami <iofg2100@gmail.com>
2015-05-16 12:24:59 +0900

    Fix application args in spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ Coveralls.wear!
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'qml'
-QML.init(%w{--platform offscreen})
+QML.init(%w{-platform offscreen})
 
 Dir.glob(Pathname(__FILE__) +'../shared/**/*.rb') do |f|
   require f

0268e58af8f04195c839811b7260b7ee2d781a34
Ryohei Ikegami <iofg2100@gmail.com>
2015-06-20 13:26:11 +0900

    Unset tick callback when not needed

--- ext/qml/dispatcher.c
+++ ext/qml/dispatcher.c
@@ -1,7 +1,6 @@
 #include "dispatcher.h"
 
 VALUE rbqml_cDispatcher;
-int callback_enabled = 1;
 
 static void tick_callback_impl() {
     VALUE dispatcher = rb_funcall(rbqml_cDispatcher, rb_intern("instance"), 0);
@@ -9,16 +8,14 @@ static void tick_callback_impl() {
 }
 
 static void tick_callback() {
-    if (__sync_fetch_and_add(&callback_enabled, 0)) {
-        rb_thread_call_with_gvl((void *(*)(void *))&tick_callback_impl, NULL);
-    }
+    rb_thread_call_with_gvl((void *(*)(void *))&tick_callback_impl, NULL);
 }
 
 static VALUE dispatcher_callback_enabled_set(VALUE self, VALUE enabled) {
     if (RTEST(enabled)) {
-        __sync_fetch_and_or(&callback_enabled, 1);
+        qmlbind_set_tick_callback(&tick_callback);
     } else {
-        __sync_fetch_and_and(&callback_enabled, 0);
+        qmlbind_set_tick_callback(NULL);
     }
     return enabled;
 }
